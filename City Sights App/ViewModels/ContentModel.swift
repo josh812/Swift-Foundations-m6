d52bd3850ca2194af61cda32dd70a42afa15af8e
Josh812 <joshua.mccrory@icloud.com>
2022-03-31 14:52:15 -0700

    - Send request to Yelp API and get data

--- City Sights App/ViewModels/ContentModel.swift
+++ City Sights App/ViewModels/ContentModel.swift
@@ -21,8 +21,7 @@ class ContentModel: NSObject, ObservableObject, CLLocationManagerDelegate {
     
     // MARK: Location Manager Delegate Methods
     func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-        if locationManager.authorizationStatus == .authorizedAlways
-            || locationManager.authorizationStatus == .authorizedWhenInUse {
+        if locationManager.authorizationStatus == .authorizedAlways || locationManager.authorizationStatus == .authorizedWhenInUse {
             locationManager.startUpdatingLocation()
         } else if locationManager.authorizationStatus == .denied {
             
@@ -30,6 +29,38 @@ class ContentModel: NSObject, ObservableObject, CLLocationManagerDelegate {
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        locationManager.stopUpdatingLocation()
+        let userLocation = locations.first
+        
+        if userLocation != nil {
+            locationManager.stopUpdatingLocation()
+            
+//            getBusinesses(category: "arts", location: userLocation!)
+            getBusinesses(category: "restaurants", location: userLocation!)
+        }
+    }
+    
+    // MARK: Yelp API Methods
+    func getBusinesses(category: String, location: CLLocation) {
+        let urlString = "https:api.yelp.com/v3/businesses/search?latitude=\(location.coordinate.latitude)&longitude=\(location.coordinate.longitude)&categories=\(category)&limit=6"
+        let URL = URL(string: urlString)
+        
+        guard URL != nil else {
+            return
+        }
+        
+        var request = URLRequest(url: URL!, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10.0)
+        
+        request.httpMethod = "GET"
+        request.addValue("Bearer sIif6iSpJqxqJYumVjS8tsgDchhH97l8BDlYiiYR7A4Oc-_I-HasKNhgNY73nitLTtxIEL4vgcKR_bAU_KoCSAoqRBQYUISV0r58wAfMfb46p3nnRlyKpPkoRvpEYnYx", forHTTPHeaderField: "Authorization")
+        
+        let session = URLSession.shared
+        
+        let dataTask = session.dataTask(with: request) { data, response, error in
+            if error == nil {
+                print(response ?? "No Response")
+            }
+        }
+        
+        dataTask.resume()
     }
 }
